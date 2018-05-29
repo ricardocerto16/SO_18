c6e1e92ef63498cec77e79adcf78b99ef0de1ff5
adrianasg122 <adrianaguedes13@gmail.com>
2018-05-29 23:54:43 +0100

    limpezas

--- parser.c
+++ parser.c
@@ -4,15 +4,12 @@
 int readlinha(int fd, char * buffer, int nbyte){
 	int i = 0;
 
-	 while(i < nbyte-1 &&
-         read(fd, buffer+i,1)>0 &&
-         buffer[i] != '\n'){
-         i++;
-    }
-    if(i>=nbyte)
-           buffer[i] = 0;
+	while(i < nbyte-1 && read(fd, buffer + i, 1 ) >0 && buffer[i] != '\n')
+        i++;
+    if (i >= nbyte)
+        buffer[i] = 0;
     else
-           buffer[i+1] = 0;
+        buffer[i+1] = 0;
 
     return i;
 }
