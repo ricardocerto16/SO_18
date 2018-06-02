2d24126e1eefd1dfacb6d1b06f34a984103e70f5
adrianasg122 <adrianaguedes13@gmail.com>
2018-06-02 22:26:51 +0100

    Main alterada para vários ficheiros

--- main.c
+++ main.c
@@ -4,19 +4,24 @@
 
 int main(int argc, char *argv[]){
 	int i, r;
+	Array a;
+	char* res = "EXECUÇÃO BEM SUCEDIDA DO FICHEIRO ";
 
 	if (argc < 2) return -1;
 
 	for (i = 1 ; i < argc ; i++) {
-		Array a = initArray(10);
+		a = initArray(10);
 		parser(argv[i],a);
 
 		r = execut(a);
 
-	if (r >= 0)	
-		file_writen(argv[i],a);
-
-	freeStruct(a);
+		if (r >= 0)	{
+			file_writen(argv[i],a);
+			printf("FICHEIRO ESCRITO COM SUCESSO -> %s\n", argv[i]);
+		}
+		else 
+			printf("FALHA NA EXECUÇÃO DO FICHEIRO -> %s\n", argv[i]);
 	}
+	freeStruct(a);
 	return 0;
 }
